9da4da93e7368e9cc55ed2105278ccc3538079d8
madprops/mc <manuelchaves@gmail.com>
2017-06-30 03:43:25 -0600

    sfx

--- app/static/app/base.js
+++ app/static/app/base.js
@@ -1042,11 +1042,11 @@ function create_about()
 	s += "You can save a sheet for future use or sharing.<br><br><br>";
 
 	s += "<span class='b2'>Shortcuts</span><br><br>";
-	s += "Up and Down arrows change the focus between lines.<br><br>";
-	s += "Tab and Shift + Tab cycle the focus between lines.<br><br>";
 	s += "Enter will focus the next available empty line or create a new one.<br><br>";
 	s += "Shift + Enter does the same but also adds the previous line's variable into the new one.<br><br>";
 	s += "Shift + Space adds the variable from the line above to the current line.<br><br>";
+	s += "Up and Down arrows change the focus between lines.<br><br>";
+	s += "Tab and Shift + Tab cycle the focus between lines.<br><br>";
 	s += "Escape clears a line, removes the line if already cleared, or closes popups.<br><br>";
 	s += "Constants and methods in the Reference will be added to the current line when clicked.";
 
