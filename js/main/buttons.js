959dfa404900b0e6854adece3e958278cee30d37
Auric Vente <mprops@protonmail.com>
2023-05-05 07:28:36 -0600

    Mods

--- js/main/buttons.js
+++ js/main/buttons.js
@@ -1,16 +1,16 @@
 App.draw_buttons = () => {
-	App.place_button(1, `Right Click: 0.1 | Middle Click: 1/1`)
-	App.place_button(2, `Right Click: 0.2 | Middle Click: 1/2`)
-	App.place_button(3, `Right Click: 0.3 | Middle Click: 1/3`)
-	App.place_button(4, `Right Click: 0.4 | Middle Click: 1/4`)
-	App.place_button(5, `Right Click: 0.5 | Middle Click: 1/5`)
-	App.place_button(6, `Right Click: 0.6 | Middle Click: 1/6`)
-	App.place_button(7, `Right Click: 0.7 | Middle Click: 1/7`)
-	App.place_button(8, `Right Click: 0.8 | Middle Click: 1/8`)
-	App.place_button(9, `Right Click: 0.9 | Middle Click: 1/9`)
-	App.place_button(0, `Right Click: 0. | Middle Click: 000`)
+	App.place_button(1)
+	App.place_button(2)
+	App.place_button(3)
+	App.place_button(4)
+	App.place_button(5)
+	App.place_button(6)
+	App.place_button(7)
+	App.place_button(8)
+	App.place_button(9)
+	App.place_button(0)
 	App.place_button(`.`)
-  App.place_button(`pi`, `Right Click: tau`)
+  App.place_button(`pi`)
 
 	App.buttons_br()
 
@@ -20,22 +20,22 @@ App.draw_buttons = () => {
 	App.place_button(`/`)
 	App.place_button(`(`)
 	App.place_button(`)`)
-	App.place_button(`^`, `Right Click: ^2 | Middle Click: ^3`)
-	App.place_button(`sqrt`, `Right Click: Cube Root | Middle Click: nth Root`)
-	App.place_button(`sin`, `Right Click: asin | Middle Click: asinh`)
-	App.place_button(`cos`, `Right Click: acos | Middle Click: acosh`)
-	App.place_button(`tan`, `Right Click: atan | Middle Click: atanh`)
+	App.place_button(`^`)
+	App.place_button(`sqrt`)
+	App.place_button(`sin`)
+	App.place_button(`cos`)
+	App.place_button(`tan`)
   App.place_button(`phi`)
 
 	App.buttons_br()
 
-	App.place_button_wider(`Add Line`, `Right Click: Add Line After | Middle Click: Add Line Before`)
+	App.place_button_wider(`Add Line`)
 	App.place_button_wider(`Clear`)
 	App.place_button_wider(`Erase`)
 	App.place_button_wider(`Undo`)
-	App.place_button(`log`, `Right Click: log2 | Middle Click: log10`)
-	App.place_button(`LN2`, `Right Click: LN10`)
-	App.place_button(`e`, `Right Click: LOG2E | Middle Click: LOG10E`)
+	App.place_button(`log`)
+	App.place_button(`LN2`)
+	App.place_button(`e`)
   App.place_button(`i`)
 
 	for (let btn of DOM.els(`.button`)) {
@@ -51,17 +51,15 @@ App.draw_buttons = () => {
 	}
 }
 
-App.place_button = (s, title = ``) => {
+App.place_button = (s) => {
 	let el = DOM.create(`button`)
-	el.title = title
 	el.classList.add(`button`)
 	el.textContent = s
 	DOM.el(`#buttons`).appendChild(el)
 }
 
-App.place_button_wider = (s, title = ``) => {
+App.place_button_wider = (s) => {
 	let el = DOM.create(`button`)
-	el.title = title
 	el.classList.add(`button`)
 	el.classList.add(`wider`)
 	el.textContent = s
@@ -73,62 +71,62 @@ App.buttons_br = () => {
 	DOM.el(`#buttons`).appendChild(el)
 }
 
-App.press = (s) => {
-	if (s === `sin`) {
-		s = `sin(`
+App.press = (value) => {
+	if (value === `sin`) {
+		value = `sin(`
 	}
-	else if (s === `cos`) {
-		s = `cos(`
+	else if (value === `cos`) {
+		value = `cos(`
 	}
-	else if (s === `tan`) {
-		s = `tan(`
+	else if (value === `tan`) {
+		value = `tan(`
 	}
-	else if (s === `sqrt`) {
-		s = `sqrt(`
+	else if (value === `sqrt`) {
+		value = `sqrt(`
 	}
-	else if (s === `log`) {
-		s = `log(`
+	else if (value === `log`) {
+		value = `log(`
 	}
-	else if (s === `Clear`) {
+	else if (value === `Clear`) {
     App.clear_input()
 		return
 	}
-	else if (s === `Erase`) {
+	else if (value === `Erase`) {
 		App.erase()
 		return
 	}
-  else if (s === `Undo`) {
+  else if (value === `Undo`) {
 		App.undo()
 		return
 	}
-	else if (s === `Add Line`) {
+	else if (value === `Add Line`) {
 		App.focus_next_or_add()
 		App.focus_input()
 		return
 	}
-	else if (s === `Up`) {
+	else if (value === `Up`) {
 		App.line_up()
 		App.focus_input()
 		return
 	}
-	else if (s === `Down`) {
+	else if (value === `Down`) {
 		App.line_down()
 		App.focus_input()
 		return
 	}
 
-	let v = App.get_var()
+	let vr = App.get_var()
 
-	if (s === v) {
+	if (value === vr) {
 		App.focus_next_or_add()
-		v = App.get_var()
+		vr = App.get_var()
 
-		if (s === v) {
+		if (value === vr) {
 			return
 		}
 	}
 
-	App.insert_text(App.focused.input, s)
+	App.insert_text(App.focused.input, value)
 }
 
 App.button_context = (button) => {
